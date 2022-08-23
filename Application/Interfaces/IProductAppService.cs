e319fb2b9268d5e8f2820b3794c53235efd32afc
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-22 23:36:53 -0300

    refactor: herda de IAppServicesBase, Add é void, Delete recebe tupla

--- Application/Interfaces/IProductAppService.cs
+++ Application/Interfaces/IProductAppService.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 
 namespace Application.Interfaces
 {
-    public interface IProductAppService
+    public interface IProductAppService : IAppServicesBase
     {
         public IEnumerable<ProductResult> GetAll();
 
         public ProductResult Get(int id);
 
-        public (bool status, string message) Add(CreateProductRequest product);
+        public void Add(CreateProductRequest product);
 
         public (bool status, string message) Update(int id, UpdateProductRequest product);
 
-        public void Delete(int id);
+        public (bool status, string message) Delete(int id);
     }
 }
