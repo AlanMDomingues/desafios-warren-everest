3b6a0907f3a3c74a6101fbd5471fb40233765f03
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-23 00:28:46 -0300

    refactor: remoção da ligação ICustomerBankInfoService, Add, Update, Delete, ValidateAlreadyExists

--- Domain.Services/Services/CustomerService.cs
+++ Domain.Services/Services/CustomerService.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.Models;
 using Domain.Services.Interfaces;
 using EntityFrameworkCore.UnitOfWork.Interfaces;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -10,14 +9,10 @@ namespace Domain.Services.Services
 {
     public class CustomerService : ServiceBase, ICustomerService
     {
-        private readonly ICustomerBankInfoService _customerBankInfoService;
-
         public CustomerService(
-            ICustomerBankInfoService customerBankInfoService,
             IUnitOfWork unitOfWork,
             IRepositoryFactory repositoryFactory)
-            : base(repositoryFactory, unitOfWork) 
-            => _customerBankInfoService = customerBankInfoService ?? throw new ArgumentNullException(nameof(customerBankInfoService));
+            : base(repositoryFactory, unitOfWork) { }
 
         public IEnumerable<Customer> GetAll()
         {
@@ -61,52 +56,36 @@ namespace Domain.Services.Services
             return result;
         }
 
-        public (bool exists, string message) Add(Customer newCustomer)
+        public void Add(Customer newCustomer)
         {
-            (bool exists, string message) = ValidateAlreadyExists(newCustomer);
-            if (exists) return (false, message);
-
             var repository = UnitOfWork.Repository<Customer>();
-            _customerBankInfoService.Add(newCustomer);
 
             repository.Add(newCustomer);
             UnitOfWork.SaveChanges();
-
-            return (true, newCustomer.Id.ToString());
         }
 
-        public (bool status, string messageResult) Update(Customer customer)
+        public void Update(Customer customer)
         {
-            (bool exists, string message) = ValidateAlreadyExists(customer);
-            if (exists) return (false, message);
-
             var repository = UnitOfWork.Repository<Customer>();
+
             repository.Update(customer);
             UnitOfWork.SaveChanges();
-
-            return (true, $"Customer for ID: {customer.Id} updated successfully");
         }
 
-        public bool Delete(int id)
+        public void Delete(int id)
         {
             var repository = UnitOfWork.Repository<Customer>();
 
-            if (!repository.Any(x => x.Id.Equals(id))) return false;
-
             repository.Remove(x => x.Id.Equals(id));
-            return true;
         }
 
-        private (bool exists, string message) ValidateAlreadyExists(Customer customer)
+        public bool ValidateAlreadyExists(Customer customer)
         {
             var repository = RepositoryFactory.Repository<Customer>();
 
-            if (repository.Any(x => x.Id != customer.Id && (x.Email.Equals(customer.Email) || x.Cpf.Equals(customer.Cpf))))
-            {
-                return (true, "Customer already exists, please insert a new customer");
-            }
+            if (repository.Any(x => x.Email.Equals(customer.Email) || x.Cpf.Equals(customer.Cpf))) return true;
 
-            return (default, customer.Id.ToString());
+            return false;
         }
     }
 }
