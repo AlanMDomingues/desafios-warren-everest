e34450b5859c10f03289bb2692272c4280fdae10
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-22 23:32:09 -0300

    refactor: herda de IAppServicesBase. Add, Update, Delete recebem tuplas feat: GetWithoutMap, GetPortfolioByCustomer, GetAllPortfoliosByCustomer, TransferMoneyToPortfolio, TransferMoneyToAccountBalance

--- Application/Interfaces/IPortfolioAppService.cs
+++ Application/Interfaces/IPortfolioAppService.cs
@@ -1,15 +1,30 @@
 ﻿using Application.Models.Requests;
 using Application.Models.Response;
+using Domain.Models;
 using System.Collections.Generic;
 
 namespace Application.Interfaces
 {
-    public interface IPortfolioAppService
+    public interface IPortfolioAppService : IAppServicesBase
     {
         public IEnumerable<PortfolioResult> GetAll(int id);
+
         public PortfolioResult Get(int id);
-        public bool Add(CreatePortfolioRequest portfolio);
-        public bool Update(int id, UpdatePortfolioRequest portfolio);
-        public bool Delete(int id);
+
+        public Portfolio GetWithoutMap(int id);
+
+        public Portfolio GetPortfolioByCustomer(int customerId, int id);
+
+        public IEnumerable<Portfolio> GetAllPortfoliosByCustomer(int customerId);
+
+        public (bool status, string message) Add(CreatePortfolioRequest portfolio);
+
+        public (bool status, string message) Update(int id, UpdatePortfolioRequest portfolio);
+
+        public (bool status, string message) Delete(int id);
+
+        public (bool status, string message) TransferMoneyToPortfolio(int customerBankInfoId, int portfolioId, decimal cash);
+
+        public (bool status, string message) TransferMoneyToAccountBalance(int customerBankInfoId, int portfolioId, decimal cash);
     }
 }
