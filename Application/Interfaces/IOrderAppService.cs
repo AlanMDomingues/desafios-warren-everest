fc86981d7f84f4c930865a6e9e99de0393b3c07a
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-22 23:29:44 -0300

    refactor: herda de IAppServicesBase feat: GetAll refactor: Add recebe tupla e novos parâmetros

--- Application/Interfaces/IOrderAppService.cs
+++ Application/Interfaces/IOrderAppService.cs
@@ -1,10 +1,15 @@
-﻿using Application.Models.Response;
+﻿using Application.Models.Requests;
+using Application.Models.Response;
+using System.Collections.Generic;
 
 namespace Application.Interfaces
 {
-    public interface IOrderAppService
+    public interface IOrderAppService : IAppServicesBase
     {
+        public IEnumerable<OrderResult> GetAll(int id);
+
         public OrderResult Get(int id);
-        public bool Add(int portfolioId, int productId, int quotes);
+
+        public (bool status, string message) Add(int customerBankInfoId, CreateOrderRequest orderRequest);
     }
 }
