eb8d42fe0efa3371b207eed5a6db520d4bdf5a0a
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-22 23:28:29 -0300

    refactor: herda de IAppServicesBase feat: GetWithoutMap, Add, Update, MoneyDeposit, WithdrawMoney

--- Application/Interfaces/ICustomerBankInfoAppService.cs
+++ Application/Interfaces/ICustomerBankInfoAppService.cs
@@ -1,9 +1,20 @@
 ﻿using Application.Models.Response;
+using Domain.Models;
 
 namespace Application.Interfaces
 {
-    public interface ICustomerBankInfoAppService
+    public interface ICustomerBankInfoAppService : IAppServicesBase
     {
         public CustomerBankInfoResult Get(int id);
+
+        public CustomerBankInfo GetWithoutMap(int id);
+
+        public void Add(int customerId);
+
+        public void Update(CustomerBankInfo customerBankInfo);
+
+        public (bool status, string message) MoneyDeposit(int id, decimal cash);
+
+        public (bool status, string message) WithdrawMoney(int id, decimal cash);
     }
 }
