6a3b693cca194d38d9dda5558f22c5145593a741
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-23 00:22:04 -0300

    feat: Add, Update

--- Domain.Services/Interfaces/ICustomerBankInfoService.cs
+++ Domain.Services/Interfaces/ICustomerBankInfoService.cs
@@ -5,6 +5,9 @@ namespace Domain.Services.Interfaces
     public interface ICustomerBankInfoService : IServiceBase
     {
         public CustomerBankInfo Get(int id);
-        public void Add(Customer customer);
+
+        public void Add(int customerId);
+
+        public void Update(CustomerBankInfo customerBankInfo);
     }
 }
