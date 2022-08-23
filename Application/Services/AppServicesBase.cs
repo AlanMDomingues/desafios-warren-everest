298611bd210bef7c96af1d4d83d8ec7c9d9ddb10
Álan Martin Domingues <alan.domingues@warren.com.br>
2022-08-22 23:24:49 -0300

    feat: AppServicesBase, IAppServicesBase

---
+++ Application/Services/AppServicesBase.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using System;
+
+namespace Application.Services
+{
+    public abstract class AppServicesBase : IAppServicesBase
+    {
+        protected IMapper Mapper { get; }
+        public AppServicesBase(IMapper mapper)
+            => Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+}
